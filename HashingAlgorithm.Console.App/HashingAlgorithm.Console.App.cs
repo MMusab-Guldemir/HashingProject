fb3269fcb23695f08467e889fa27f15110d1542b
Muhammed <mmusabgldmr@gmail.com>
2025-11-04 22:51:20 +0300

    HashingAlgorithm.Console.App

---
+++ HashingAlgorithm.Console.App/HashingAlgorithm.Console.App.cs
@@ -0,0 +1,36 @@
+﻿using HashingAlgorithm.Library;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace HashingAlgorithm.Console.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * In this example, I believe it is a straightforward
+               call or invoke of the method
+             * First, you just needed to pass the
+               arguments such as plainText, the raw data
+               in this case, 'Hello World'
+             * Second, you can choose from
+               the different algorithms available:
+               MD5, SHA1, SHA256, SHA384, and SHA
+             */
+
+            string hashResult =
+                   HashingHelper.ComputeHashInLowerCase(plainText,
+                                      HashingClassAlgorithms.MD5);
+
+            System.Console.WriteLine(hashResult);
+            System.Console.ReadLine();
+        }
+    }
+}
